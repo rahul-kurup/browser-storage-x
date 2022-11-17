05b8da6c19957d96fd50227cf300598a01ed4fd6
rahul kurup <raulsrule@gmail.com>
2022-11-17 19:24:23 +0530

    fix: dev icon not showing up

--- src/manifest/base/icons.js
+++ src/manifest/base/icons.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const path = require('path');
 const { isDevMode } = require('./helper');
 
+const assetPath = ['assets', 'images', 'logo'].join('/');
+
 const icons = [16, 24, 32, 48, 96, 128].reduce((a, c) => {
-  const coloredLogo = `assets/images/logo/${c}-colored.png`;
-  const simpleLogo = `assets/images/logo/${c}.png`;
-  const existColoredLogo = fs.existsSync(coloredLogo);
-  const logoToUse = isDevMode && existColoredLogo ? coloredLogo : simpleLogo;
+  const simpleLogo = `${assetPath}/${c}.png`;
+  const coloredLogo = `${assetPath}/${c}-colored.png`;
+  const coloredLogoFullpath = path.join(process.cwd(), 'src', coloredLogo);
+  const coloredLogoExists = fs.existsSync(coloredLogoFullpath);
+  const logoToUse = isDevMode && coloredLogoExists ? coloredLogo : simpleLogo;
 
   return {
     ...a,
